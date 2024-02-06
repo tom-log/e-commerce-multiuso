f67061bc735cd31e2122f88a5db8cd7d0e800ed7
tom-log <nizekaos@gmail.com>
2024-02-06 12:40:00 -0300

    inicio do projeto

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// VARIÁVEIS GLOBAIS
+$email = 'lojaportalhugo@gmail.com';
+$telefone = '(31) 3333-3333';
+$whatsapp = '(31) 9999-9999';
+$whatsapp_link = '5531975275084';
+$nome_loja = 'Boutique Freitas';
+$texto_destaque = 'Todos os Produtos em Promoção!';
